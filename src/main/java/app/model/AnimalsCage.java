d2b67f953c25c34f2c154c99ebbde32e415fce49
VitaliyKrivoshchapov <69453241+VitaliyKrivoshchapov@users.noreply.github.com>
2020-12-25 21:17:17 +0500

    поправил тесты

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -15,6 +15,10 @@ public class AnimalsCage {
     @Qualifier("dog")
     private Animal animal;
 
+    public Timer getTimer() {
+        return timer;
+    }
+
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
@@ -22,4 +26,5 @@ public class AnimalsCage {
         System.out.println(timer.getTime());
         System.out.println("________________________");
     }
+
 }
